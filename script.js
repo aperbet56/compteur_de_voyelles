ca852f091a80ef6641f0f3dde608e463b97d9c3c
Annabelle Perbet <annabelleperbet@yahoo.fr>
2024-04-13 18:42:12 +0200

    Ajout du fichier script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Récupération des différents éléments
+const vowelsCount = document.querySelector(".vowels__count");
+
+// Création de la fonction counterVowels qui va permettre de compter les voyelles
+const counterVowels = (str) => {
+  str = str.toLowerCase();
+
+  // Création des variables
+  let counter = 0;
+  let vowelsArray = ["a", "e", "i", "o", "u", "y"];
+
+  for (let letter of str) {
+    if (vowelsArray.includes(letter)) {
+      counter++; // incrémentation du compteur
+    }
+  }
+  vowelsCount.textContent = counter;
+};
